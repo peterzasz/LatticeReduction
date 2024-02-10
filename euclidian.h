e458bbc29924091f5898d7d2a82d2f224f15910b
Peter Fazekas <peterzasz@gmail.com>
2024-02-10 12:54:53 +0100

    Add classes euclidian, fraction

---
+++ euclidian.h
@@ -0,0 +1,223 @@
+#ifndef EUCLIDIAN
+#define EUCLIDIAN
+
+#include<vector>
+
+template <typename T>
+class Vector
+{
+	public:
+		Vector( std::vector<T> _v ) : v(_v), dim(_v.size()) {};
+		
+		T &at(int i)
+		{
+			if( dim <= i || i < 0 )
+			{
+				throw("Index is out of range!");
+			}
+			else
+			{
+				return v[i];
+			}
+		}
+		
+		int getDim() { return dim; }
+		
+		template <typename U>
+		Vector<U> times( U c )
+		{
+			Vector<U> new_vector(v);
+			for( int i = 0; i < dim; ++i )
+			{
+				new_vector.at(i) = new_vector.at(i)*c;
+			}
+			
+			return new_vector;
+		}
+		
+		template <typename U>
+		Vector<U> div( U c )
+		{
+			Vector<U> new_vector(v);
+			for( int i = 0; i < dim; ++i )
+			{
+				new_vector.at(i) = new_vector.at(i)/c;
+			}
+			
+			return Vector<U>(new_vector);
+		}
+		
+		template<typename U>
+		Vector<U> add( Vector<U> &u )
+		{
+			if( u.getDim() != dim )
+			{
+				throw("Two vectors are not of the same dimension!");
+			}
+			else
+			{
+				Vector<U> new_vector(u);
+				
+				for( int i = 0; i < dim; ++i )
+				{
+					new_vector.at(i) = v[i] + new_vector.at(i);
+				}
+				
+				return new_vector;
+			}
+		}
+		
+		template<typename U>
+		Vector<U> minus( Vector<U> &u )
+		{
+			if( u.getDim() != dim )
+			{
+				throw("Two vectors are not of the same dimension!");
+			}
+			else
+			{
+				Vector<U> new_vector(u);
+				
+				for( int i = 0; i < dim; ++i )
+				{
+					new_vector.at(i) = v[i] - new_vector.at(i);
+				}
+				
+				return new_vector;
+			}
+		}
+		
+		template<typename U>
+		U product( Vector<U> &u )
+		{
+			if( u.getDim() != dim )
+			{
+				throw("Two vectors are not of the same dimension!");
+			}
+			else
+			{
+				U prod;
+				
+				for( int i = 0; i < dim; ++i )
+				{
+					prod = prod + u.at(i) * v[i];
+				}
+				
+				return prod;
+			}
+		}
+	
+	private:
+		std::vector<T> v;
+		int dim;
+};
+
+template <typename T>
+std::ostream& operator<<( std::ostream& strm, Vector<T> v )
+{
+	strm << '[';
+	
+	for( int i = 0; i < v.getDim()-1; ++i )
+	{
+		strm << v.at(i) << ", ";
+	}
+	
+	strm << v.at(v.getDim()-1) << ']';
+	
+	return strm;
+}
+
+template <typename T>
+Vector<T> operator+( Vector<T>& u, Vector<T>& v )
+{
+	return u.add(v);
+}
+
+template <typename T>
+Vector<T> operator-( Vector<T>& u, Vector<T>& v )
+{ 
+	return u.minus(v);
+}
+
+template <typename T>
+T operator*( Vector<T>& u, Vector<T>& v )
+{ 
+	return u.product(v);
+}
+
+template <typename T>
+Vector<T> operator*( T c, Vector<T>& v )
+{ 
+	return v.times(c);
+}
+
+template <typename T>
+Vector<T> operator*( Vector<T>& v, T c )
+{ 
+	return v.times(c);
+}
+
+template <typename T>
+Vector<T> operator*( int c, Vector<T>& v )
+{ 
+	T cc(c);
+	return v.times(cc);
+}
+
+template <typename T>
+Vector<T> operator*( Vector<T>& v, int c )
+{ 
+	T cc(c);
+	return v.times(cc);
+}
+
+template <typename T>
+Vector<T> operator/( Vector<T>& v, T c )
+{ 
+	return v.div(c);
+}
+
+template <typename T>
+bool operator==( Vector<T>& u, Vector<T>& v )
+{
+	if( u.getDim() != v.getDim() )
+	{
+		return false;
+	}
+	else
+	{
+		bool same = true;
+		
+		for( int i = 0; i < u.getDim(); ++i )
+		{
+			if( v.at(i) != u.at(i) )
+			{
+				same = false;
+				break;
+			}
+		}
+		
+		return same;
+	}
+}
+
+template <typename T>
+bool operator!=( Vector<T> u, Vector<T> v ) { return !(u == v); }
+
+class Lattice
+{
+
+};
+
+class Base_reduction
+{
+
+};
+
+class SVP_solver
+{
+
+};
+
+
+#endif
